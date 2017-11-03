447fc1164b6402aed30552376c3f14adfa2a99f3
Or Israeli <tur103103@gmail.com>
2017-11-03 18:44:45 +0200

    cloud synchronization

---
+++ automatically_cloud_synchronization.py
@@ -0,0 +1,83 @@
+"""
+Author          :   Or Israeli
+FileName        :   automatically_cloud_synchronization.py
+Date            :   3.11.17
+Version         :   1.0
+Description     :   A program that synchronizes the files from
+                    the system's folder automatically with the cloud.
+"""
+from constants import *
+import win32file
+from win32con import *
+import socket
+
+
+def automatically_cloud_synchronization():
+    system_folder_creation = win32file.CreateFile(SYSTEM_FOLDER, FILE_LIST_DIRECTORY, FILE_SHARE_READ,
+                                                  None, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, None)
+    waiting_for_change(system_folder_creation)
+
+
+def waiting_for_change(system_folder_creation):
+    """
+
+    The function waits till a change appeared in the system's folder.
+    A new file was added, a file was deleted or a file was modified
+    by the user. The function catches all the cases.
+
+    Args:
+        system_folder_creation (CreateFile object): The system folder for detecting changes on.
+
+    """
+    while True:
+        action, file_name = win32file.ReadDirectoryChangesW(system_folder_creation, BUFFER, True,
+                                                            FILE_NOTIFY_CHANGE_FILE_NAME |
+                                                            FILE_NOTIFY_CHANGE_LAST_WRITE)[0]
+        synchronize_the_changes(action, file_name)
+
+
+def synchronize_the_changes(action, file_name):
+    """
+
+    The function receives the change that occurred
+    and synchronize it with the cloud.
+
+    Args:
+        action (str): The action number of the change. file was added,
+                      file was deleted or file was modified.
+        file_name (string): The name of the file that was changed.
+
+    """
+
+
+
+
+def create_system_folder():
+    """
+
+    The function checks if the system folder is exists in the user's
+    computer. If not it creates a new one.
+
+    """
+    if not os.path.exists(SYSTEM_FOLDER):
+        os.mkdir(SYSTEM_FOLDER)
+
+
+def open_connection_with_the_cloud():
+    """
+
+    The function opens a socket connection with the server
+    cloud for synchronization.
+
+    Returns:
+        (socket): the socket connection with the cloud.
+
+    """
+    client_socket = socket.socket()
+    client_socket.connect((CLOUD_HOST, CLOUD_PORT))
+    return client_socket
+
+
+if __name__ == '__main__':
+    create_system_folder()
+    automatically_cloud_synchronization()
---
+++ constants.py
@@ -0,0 +1,9 @@
+import os
+
+FOLDER_NAME = "c-o-s folder"
+SYSTEM_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), FOLDER_NAME)
+FILE_LIST_DIRECTORY = 1
+BUFFER = 1024
+
+CLOUD_HOST = "127.0.0.1"
+CLOUD_PORT = 8820
