b533539c1c54d70653cac19811214663801f3725
Or Israeli <tur103103@gmail.com>
2018-01-23 17:52:33 +0200

    finish calculator

--- softwares/calculator/calculator.py
+++ softwares/calculator/calculator.py
@@ -16,8 +16,11 @@ class CalculatorScreen(Screen):
 
     upper_string = ""
     lower_string = "0"
+    calculation = "0"
+    last_operator = ""
     arithmetic = False
     equal = False
+    errors_list = ["Invalid Input", "Can Not Divide By Zero"]
 
     def add_widget(self, *args):
         if 'content' in self.ids:
@@ -25,44 +28,61 @@ class CalculatorScreen(Screen):
         return super(CalculatorScreen, self).add_widget(*args)
 
     def digit_clicked(self, digit):
-        if CalculatorScreen.arithmetic or CalculatorScreen.equal:
-            CalculatorScreen.lower_string = "0"
-        if CalculatorScreen.lower_string == "0" and digit != ".":
-            CalculatorScreen.lower_string = digit
-        else:
-            CalculatorScreen.lower_string += digit
-        CalculatorScreen.arithmetic = False
-        CalculatorScreen.equal = False
-        self.clicked()
+        if CalculatorScreen.lower_string not in CalculatorScreen.errors_list:
+            if digit != "." or CalculatorScreen.lower_string[-1] != ".":
+                if CalculatorScreen.arithmetic or CalculatorScreen.equal:
+                    CalculatorScreen.lower_string = "0"
+                if CalculatorScreen.lower_string == "0" and digit != ".":
+                    CalculatorScreen.lower_string = digit
+                else:
+                    CalculatorScreen.lower_string += digit
+                CalculatorScreen.arithmetic = False
+                CalculatorScreen.equal = False
+                self.clicked()
 
     def arithmetic_clicked(self, arithmetic):
-        if CalculatorScreen.arithmetic:
-            if arithmetic != CalculatorScreen.upper_string[-2]:
-                CalculatorScreen.upper_string = CalculatorScreen.upper_string[:-2]
-                CalculatorScreen.upper_string += " " + arithmetic
-        else:
-            CalculatorScreen.upper_string += " " + CalculatorScreen.lower_string
-            CalculatorScreen.lower_string = self.arithmetic_calculate()
-            CalculatorScreen.upper_string += " " + arithmetic
-            CalculatorScreen.arithmetic = True
-            CalculatorScreen.equal = False
-        self.clicked()
+        if CalculatorScreen.lower_string not in CalculatorScreen.errors_list:
+            if CalculatorScreen.arithmetic:
+                if arithmetic != CalculatorScreen.upper_string[-2]:
+                    CalculatorScreen.upper_string = CalculatorScreen.upper_string[:-2]
+                    CalculatorScreen.upper_string += " " + arithmetic
+                    CalculatorScreen.last_operator = arithmetic
+            else:
+                if CalculatorScreen.last_operator == "/" and CalculatorScreen.lower_string == "0":
+                    self.clear_all_clicked()
+                    CalculatorScreen.lower_string = CalculatorScreen.errors_list[1]
+                else:
+                    CalculatorScreen.upper_string += " " + CalculatorScreen.lower_string
+                    CalculatorScreen.lower_string = self.arithmetic_calculate()
+                    CalculatorScreen.last_operator = arithmetic
+                    CalculatorScreen.upper_string += " " + arithmetic
+                    CalculatorScreen.arithmetic = True
+                    CalculatorScreen.equal = False
+            self.clicked()
 
     def equal_clicked(self):
-        CalculatorScreen.upper_string += " " + CalculatorScreen.lower_string
-        CalculatorScreen.lower_string = self.arithmetic_calculate()
-        CalculatorScreen.upper_string = ""
-        CalculatorScreen.equal = True
-        self.clicked()
+        if CalculatorScreen.lower_string not in CalculatorScreen.errors_list:
+            if CalculatorScreen.last_operator == "/" and CalculatorScreen.lower_string == "0":
+                self.clear_all_clicked()
+                CalculatorScreen.lower_string = CalculatorScreen.errors_list[1]
+            else:
+                CalculatorScreen.upper_string += " " + CalculatorScreen.lower_string
+                CalculatorScreen.lower_string = self.arithmetic_calculate()
+                CalculatorScreen.upper_string = ""
+                CalculatorScreen.equal = True
+                CalculatorScreen.arithmetic = False
+                CalculatorScreen.last_operator = ""
+            self.clicked()
 
     def back_clicked(self):
-        if not CalculatorScreen.arithmetic and not CalculatorScreen.equal:
-            if CalculatorScreen.lower_string != "0":
-                if len(CalculatorScreen.lower_string):
-                    CalculatorScreen.lower_string = CalculatorScreen.lower_string[:-1]
-                    if not len(CalculatorScreen.lower_string):
-                        CalculatorScreen.lower_string = "0"
-        self.clicked()
+        if CalculatorScreen.lower_string not in CalculatorScreen.errors_list:
+            if not CalculatorScreen.arithmetic and not CalculatorScreen.equal:
+                if CalculatorScreen.lower_string != "0":
+                    if len(CalculatorScreen.lower_string):
+                        CalculatorScreen.lower_string = CalculatorScreen.lower_string[:-1]
+                        if not len(CalculatorScreen.lower_string):
+                            CalculatorScreen.lower_string = "0"
+            self.clicked()
 
     def clear_clicked(self):
         CalculatorScreen.lower_string = "0"
@@ -71,35 +91,55 @@ class CalculatorScreen(Screen):
     def clear_all_clicked(self):
         CalculatorScreen.lower_string = "0"
         CalculatorScreen.upper_string = ""
+        CalculatorScreen.calculation = "0"
+        CalculatorScreen.last_operator = ""
+        CalculatorScreen.arithmetic = False
+        CalculatorScreen.equal = False
         self.clicked()
 
     def negative_clicked(self):
-        if CalculatorScreen.lower_string[0] == "-":
-            CalculatorScreen.lower_string = CalculatorScreen.lower_string[1:]
-        else:
-            CalculatorScreen.lower_string = "-" + CalculatorScreen.lower_string
-        CalculatorScreen.arithmetic = False
-        self.clicked()
+        if CalculatorScreen.lower_string not in CalculatorScreen.errors_list:
+            if CalculatorScreen.lower_string[0] == "-":
+                CalculatorScreen.lower_string = CalculatorScreen.lower_string[1:]
+            else:
+                CalculatorScreen.lower_string = "-" + CalculatorScreen.lower_string
+            CalculatorScreen.arithmetic = False
+            self.clicked()
 
     def square_clicked(self):
-        CalculatorScreen.lower_string = str(math.sqrt(float(CalculatorScreen.lower_string)))
-        CalculatorScreen.arithmetic = False
-        self.clicked()
+        if CalculatorScreen.lower_string not in CalculatorScreen.errors_list:
+            if float(CalculatorScreen.lower_string) < 0:
+                self.clear_all_clicked()
+                CalculatorScreen.lower_string = CalculatorScreen.errors_list[0]
+            else:
+                CalculatorScreen.lower_string = str(math.sqrt(float(CalculatorScreen.lower_string)))
+                CalculatorScreen.arithmetic = False
+            self.clicked()
 
     def one_divided_by_clicked(self):
-        CalculatorScreen.lower_string = str(1 / float(CalculatorScreen.lower_string))
-        CalculatorScreen.arithmetic = False
-        self.clicked()
+        if CalculatorScreen.lower_string not in CalculatorScreen.errors_list:
+            if CalculatorScreen.lower_string == "0":
+                self.clear_all_clicked()
+                CalculatorScreen.lower_string = CalculatorScreen.errors_list[1]
+            else:
+                CalculatorScreen.lower_string = str(1 / float(CalculatorScreen.lower_string))
+                CalculatorScreen.arithmetic = False
+            self.clicked()
 
     def percent_clicked(self):
-        if CalculatorScreen.upper_string:
-            CalculatorScreen.lower_string = str(eval(CalculatorScreen.upper_string[:-2]) / 100 * float(CalculatorScreen.lower_string))
-            CalculatorScreen.arithmetic = False
-        self.clicked()
+        if CalculatorScreen.lower_string not in CalculatorScreen.errors_list:
+            if CalculatorScreen.upper_string:
+                CalculatorScreen.lower_string = str(float(CalculatorScreen.calculation) / 100 * float(CalculatorScreen.lower_string))
+                CalculatorScreen.arithmetic = False
+            self.clicked()
 
     @staticmethod
     def arithmetic_calculate():
-        return str(eval(CalculatorScreen.upper_string))
+        if CalculatorScreen.last_operator:
+            CalculatorScreen.calculation = str(eval(CalculatorScreen.calculation + CalculatorScreen.last_operator + CalculatorScreen.lower_string))
+        else:
+            CalculatorScreen.calculation = CalculatorScreen.lower_string
+        return CalculatorScreen.calculation
 
     def clicked(self):
         self.upper_string.text = CalculatorScreen.upper_string
